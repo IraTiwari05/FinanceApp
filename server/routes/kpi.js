57dabf149e592dd7c90ea1812d93469ad6df86c0
IraTiwari05 <iratiwari2003@gmail.com>
2024-06-13 23:12:35 +0530

    Add server files

--- server/routes/kpi.js
+++ server/routes/kpi.js
@@ -20,19 +20,3 @@ router.get("/kpis", async (req, res) => {
 export default router;
 
 
-/*import express from "express";
-import KPI from "../models/KPI.js";
-
-const router = express.Router();
-
-router.get("/kpis", async (req, res) => {
-  try {
-    const kpis = await KPI.find();
-   
-    res.status(200).json(kpis);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-});
-
-export default router; // ES module export*/
