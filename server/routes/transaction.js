dbf00d6c77095e071ecc3bc6b660534a501f5158
IraTiwari05 <iratiwari2003@gmail.com>
2024-06-13 23:15:10 +0530

    Add server files

--- server/routes/transaction.js
+++ server/routes/transaction.js
@@ -7,12 +7,12 @@ const router = express.Router();
 
 router.get("/transactions", async (req, res) => {
   try {
-    {/*await Transaction.insertMany(convertedTransactions);*/}
+
 
     const transactions = await Transaction.find()
     .limit(50)
     .sort({createOn:-1});
-   // Log fetched data
+   
     res.status(200).json(transactions);
   } catch (error) {
     res.status(404).json({ message: error.message });
