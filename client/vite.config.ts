0168c4995d6ebe2c192b4796bcef3b563b5d34db
IraTiwari05 <iratiwari2003@gmail.com>
2024-06-14 21:31:13 +0530

    Add client change

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  server: {
+    port: Number(process.env.PORT) || 3000, // Ensure PORT is parsed as a number
+  },
+  define: {
+    'process.env': process.env,
+  },
+});
+
+
+
+
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
+
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +32,4 @@ export default defineConfig({
   resolve:{
     alias:[{find:"@",replacement:path.resolve(__dirname,"srx")}]
   }
-})
+})*/
