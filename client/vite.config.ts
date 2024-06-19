992c8e653324e2b42dc1119a140f5ec89dab1c84
IraTiwari05 <iratiwari2003@gmail.com>
2024-06-19 11:26:59 +0530

    client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -21,18 +21,3 @@ export default defineConfig({
 
 
 
-
-/*import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
-
-
-
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve:{
-    alias:[{find:"@",replacement:path.resolve(__dirname,"srx")}]
-  }
-})*/
